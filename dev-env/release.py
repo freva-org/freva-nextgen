a5c069347d868c6f69c39e76e59d677c91ba029e
antarcticrainforest <martin.bergemann@posteo.org>
2024-03-18 01:50:20 +0100

    Tix typo

--- dev-env/release.py
+++ dev-env/release.py
@@ -50,16 +50,11 @@ class Release:
 
     version_pattern: str = r'__version__\s*=\s*["\'](\d+\.\d+\.\d+)["\']'
 
-    def __init__(
-        self, package_name: str, repo_dir: str, branch: str = "main"
-    ) -> None:
-
+    def __init__(self, package_name: str, repo_dir: str, branch: str = "main") -> None:
         self.branch = branch
         self.package_name = package_name
         self.repo_dir = Path(repo_dir)
-        logger.info(
-            "Searching for packages/config with the name: %s", package_name
-        )
+        logger.info("Searching for packages/config with the name: %s", package_name)
         logger.debug("Reading current git config")
         self.git_config = (
             Path(git.Repo(search_parent_directories=True).git_dir) / "config"
@@ -82,9 +77,7 @@ class Release:
         try:
             # Get the latest tag on the main branch
             return Version(
-                repo.git.describe("--tags", "--abbrev=0", self.branch).lstrip(
-                    "v"
-                )
+                repo.git.describe("--tags", "--abbrev=0", self.branch).lstrip("v")
             )
         except git.exc.GitCommandError:
             logger.debug("No tag found")
@@ -124,7 +117,6 @@ class Release:
         raise ValueError("Could not find version")
 
     def _clone_repo_from_franch(self, branch: str = "main") -> None:
-
         logger.debug(
             "Cloning repository from %s with branch %s to %s",
             self.repo_url,
@@ -140,7 +132,7 @@ class Release:
         if not self._change_lock_file.is_file():
             raise Exit(
                 "Could not find change log file. "
-                "Create one first and push it to the {self.branch}"
+                f"Create one first and push it to the {self.branch} branch."
             )
         if f"v{self.version}" not in self._change_lock_file.read_text("utf-8"):
             raise Exit(
@@ -155,9 +147,7 @@ class Release:
     @cached_property
     def _change_lock_file(self) -> Path:
         """Find the change lock file."""
-        for prefix, suffix in product(
-            ("changelog", "whats-new"), (".rst", ".md")
-        ):
+        for prefix, suffix in product(("changelog", "whats-new"), (".rst", ".md")):
             for search_pattern in (prefix, prefix.upper()):
                 glob_pattern = f"{search_pattern}{suffix}"
                 logger.debug("Searching for %s", glob_pattern)
@@ -184,9 +174,7 @@ class Release:
         head = cloned_repo.head.reference
         message = f"Create a release for v{self.version}"
         try:
-            cloned_repo.create_tag(
-                f"v{self.version}", ref=head, message=message
-            )
+            cloned_repo.create_tag(f"v{self.version}", ref=head, message=message)
             cloned_repo.git.push("--tags")
         except git.GitCommandError as error:
             raise Exit("Could not create tag: {}".format(error))
@@ -205,9 +193,7 @@ class Release:
             "deploy", help="Update the version in the deployment repository"
         )
         for _parser in tag_parser, deploy_parser:
-            _parser.add_argument(
-                "name", help="The name of the software/package."
-            )
+            _parser.add_argument("name", help="The name of the software/package.")
             _parser.add_argument(
                 "-v",
                 "--verbose",
