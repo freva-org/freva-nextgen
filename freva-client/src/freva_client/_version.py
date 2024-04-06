c0892ee14f01752932da7485f320212284ce1af3
antarcticrainforest <martin.bergemann@posteo.org>
2024-04-06 05:24:58 +0200

    Outsource __version__ to prevent circular imports.

---
+++ freva-client/src/freva_client/_version.py
@@ -0,0 +1,3 @@
+"""Define the version of the client software."""
+
+__version__ = "2404.0.0"
---
+++ freva-client/src/freva_databrowser/_version.py
@@ -0,0 +1,5 @@
+"""Import the version from the freva_client package."""
+
+from freva_client._version import __version__
+
+__all__ = ["__version__"]
