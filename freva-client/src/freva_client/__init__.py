ba8fb354e4ee7c530607224b2f500e05ee7c80f3
antarcticrainforest <martin.bergemann@posteo.org>
2024-10-17 14:31:36 +0200

    Add setuptools to deps.

--- freva-client/src/freva_client/__init__.py
+++ freva-client/src/freva_client/__init__.py
@@ -17,5 +17,5 @@ need to apply data analysis plugins, please visit the
 from .auth import authenticate
 from .query import databrowser
 
-__version__ = "2408.0.0"
+__version__ = "2410.0.0"
 __all__ = ["authenticate", "databrowser", "__version__"]
