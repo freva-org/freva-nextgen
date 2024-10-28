052441ec749c23459259fe022ce1087e3df7afcf
Mo <hadizadeh@dkrz.de>
2024-10-28 19:16:18 +0100

    bump pre-release version

--- freva-client/src/freva_client/__init__.py
+++ freva-client/src/freva_client/__init__.py
@@ -17,5 +17,5 @@ need to apply data analysis plugins, please visit the
 from .auth import authenticate
 from .query import databrowser
 
-__version__ = "2410.0.0-beta5"
+__version__ = "2410.0.0-beta6"
 __all__ = ["authenticate", "databrowser", "__version__"]
