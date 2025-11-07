aee2a0118d95ee5b955bab03d19f99fd2a182057
Mo <hadizadeh@dkrz.de>
2025-11-07 20:25:18 +0100

    add a support for remote data

--- freva-data-portal-worker/src/data_portal_worker/backends/__init__.py
+++ freva-data-portal-worker/src/data_portal_worker/backends/__init__.py
@@ -4,13 +4,9 @@ from urllib.parse import urlparse
 
 import xarray as xr
 
-from .posix import load_posix
+from .backend import load_data
 
 
 def load_data(inp_path: str) -> xr.Dataset:
     """Open a datasets."""
-
-    parsed_url = urlparse(inp_path)
-
-    implemented_methods = {"file": load_posix, "": load_posix}
-    return implemented_methods[parsed_url.scheme](inp_path)
+    return load_data(inp_path)
