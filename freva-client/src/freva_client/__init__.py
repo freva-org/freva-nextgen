ed2994b56b9228d13a6d6b26b9d1cacfb4573b63
antarcticrainforest <martin.bergemann@posteo.org>
2025-11-10 08:02:15 -0700

    Bump versions.

--- freva-client/src/freva_client/__init__.py
+++ freva-client/src/freva_client/__init__.py
@@ -18,5 +18,5 @@ official documentation: https://freva-org.github.io/freva-legacy
 from .auth import authenticate
 from .query import databrowser
 
-__version__ = "2510.0.1"
+__version__ = "2510.1.0"
 __all__ = ["authenticate", "databrowser", "__version__"]
